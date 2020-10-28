ee210197792aef054753d323d6c7edf1960e01a1
amen-souissi <amen.souissi@decathlon.com>
2020-10-28 23:29:27 +0100

    chore(versions): upgrade package.json versions, react-query to v3.2.0-beat-14

--- src/queryClientProvider/QueryClientProvider.svelte
+++ src/queryClientProvider/QueryClientProvider.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
   import { setContext, onMount, onDestroy } from 'svelte'
 
-  import {
-    DefaultOptions,
-    MutationCache,
-    QueryCache,
-    QueryClient,
-  } from '../queryCore'
+  import { MutationCache, QueryCache, QueryClient } from '../queryCore'
+  import type { DefaultOptions } from '../queryCore'
 
   // Props with default values
   export let queryCache = new QueryCache()
